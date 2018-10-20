e326598bfa2a4ef8fd17d43d943ce1cbfad0b35f
Ishani <ishanigupta27@github.com>
2018-10-20 13:07:24 -0700

    indenting topo

--- mininet/util/topo.sh
+++ mininet/util/topo.sh
@@ -5,27 +5,27 @@ from mininet.net import Mininet
 from mininet.cli import CLI
 
 class FirstTopology(Topo):
-"""
-A basic topology of 3 hosts and 1 switch
-"""
+	"""
+	A basic topology of 3 hosts and 1 switch
+	"""
 
-switch = self.addSwitch('s1') ## Adds a Switch
-host1 = self.addHost('h1') ## Adds a Host
-host2 = self.addHost('h2') ## Adds a Host
-host3 = self.addHost('h3') ## Adds a Host
+	switch = self.addSwitch('s1') ## Adds a Switch
+	host1 = self.addHost('h1') ## Adds a Host
+	host2 = self.addHost('h2') ## Adds a Host
+	host3 = self.addHost('h3') ## Adds a Host
 
-self.addLink(host1, switch) ## Add a link
-self.addLink(host2, switch) ## Add a link
-self.addLink(host3, switch) ## Add a link
+	self.addLink(host1, switch) ## Add a link
+	self.addLink(host2, switch) ## Add a link
+	self.addLink(host3, switch) ## Add a link
 
 if __name__ == '__main__':
- """
- If this script is run as an executable (by chmod +x), this is
- what it will do
- """
+		"""
+		If this script is run as an executable (by chmod +x), this is
+		what it will do
+		"""
 
- topo = FirstTopology() ## Creates the topology
- net = Mininet( topo=topo ) ## Loads the topology
- net.start() ## Starts Mininet
- CLI(net)
- net.stop() ## Stops Mininet
+		topo = FirstTopology() ## Creates the topology
+		net = Mininet( topo=topo ) ## Loads the topology
+		net.start() ## Starts Mininet
+		CLI(net)
+		net.stop() ## Stops Mininet
