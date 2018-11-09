ca9f2d34c38c051607abc486af08420532291936
Ishani <ishanigupta27@github.com>
2018-11-08 17:32:01 -0800

    fixing IDS generator

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-ssh -i netproj.pem ubuntu@52.47.174.144
+ssh -R 52698:localhost:52698 -X -i netproj.pem ubuntu@35.180.39.78
+
 
---
+++ start_server.sh
@@ -0,0 +1,3 @@
+ssh -R 52698:localhost:52698 -X -i netproj.pem ubuntu@35.180.39.243
+
+
