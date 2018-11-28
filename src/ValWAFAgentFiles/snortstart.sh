bce5792b1c26929b03937ef43e4988ee9ea19974
Ishani <ishanigupta27@github.com>
2018-11-28 13:05:28 -0800

    Adding ValWAFAgent3

--- src/ValWAFAgentFiles/snortstart.sh
+++ src/ValWAFAgentFiles/snortstart.sh
@@ -2,4 +2,4 @@
 configFile=$1
 
 sudo -I
-sudo snort -i s1-eth1 -c /ConfiRepo/Snort_$configFile.conf -A unsock -N -l /tmp
+sudo snort -i s1-eth1 -c ConfiRepo/Snort_$configFile.conf -A unsock -N -l /tmp
