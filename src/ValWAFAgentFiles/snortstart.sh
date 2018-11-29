6927d01d02c816bf81953529f1f58387353fe738
Ishani <ishanigupta27@github.com>
2018-11-28 18:14:34 -0800

    Finished testing the stuff for albanusi rule set 2

--- src/ValWAFAgentFiles/snortstart.sh
+++ src/ValWAFAgentFiles/snortstart.sh
@@ -2,4 +2,4 @@
 configFile=$1
 
 sudo -I
-sudo snort -i s1-eth1 -c ConfiRepo/Snort_$configFile.conf -A unsock -N -l /tmp
+sudo snort -i s1-eth1 -c ConfiRepo/Snort_$configFile.conf -A Full -l /home/ubuntu/WebApplicationFirewall/src/ValWAFAgentFiles/logs 2>&1 | tee stdlogs.txt
